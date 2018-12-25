800a5a4396c23e522d47761a5a6c413e5be0c3ce
Claudia Mastan <claudiachen97@gmail.com>
2018-12-24 21:24:32 -0800

    added exp section, trying to implement react js

---
+++ project0/exp.js
@@ -0,0 +1,16 @@
+'use strict';
+const e = React.createElement;
+
+class expDesc extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {selected : "axxera"};
+    }
+
+    render(){
+        if(this.state.selected="axxera"){
+            return
+            "Axxera, Inc";
+        }
+    }
+}
